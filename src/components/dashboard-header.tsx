df83f88c0ebc643cc5d7039bfa73f9ad605975bf
db y99 <db@y99.vn>
2025-09-04 04:20:43 +0000

    ReferenceError: cn is not defined     at DashboardHeader (https://6000-f

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -14,6 +14,7 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { AuthContext } from '@/context/auth-context';
 import { useIsMobile } from '@/hooks/use-mobile';
+import { cn } from '@/lib/utils';
 
 
 interface DashboardHeaderProps {
